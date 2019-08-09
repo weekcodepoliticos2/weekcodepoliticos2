feb697898d1c4d8442bf267b23cad195f011e302
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-08-09 13:11:59 -0300

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'politicians#index'
+  get '/ranking', to: 'politicians#ranking'
+  get '/update_ranking', to: 'politicians#update_ranking'
 end
