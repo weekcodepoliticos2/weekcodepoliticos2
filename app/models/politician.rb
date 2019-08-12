bb6433db99318fa98833c50c5c6f4466b411016a
Rafael Prallon <rafaelprallon@gmail.com>
2019-08-11 23:56:19 -0300

    Some polishment on the code

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,9 +1,9 @@
 class Politician < ApplicationRecord
-    has_one :score
+  has_one :score
 
-    def update_rating(score)
-        self.score.rating = score.rating
-        self.score.games_played = score.games_played
-        self.score.save!
-    end
+  def update_rating(score)
+    self.score.rating = score.rating
+    self.score.games_played = score.games_played
+    self.score.save!
+  end
 end
